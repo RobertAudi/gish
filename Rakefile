6cab8e10f63610cb31441f50034f3da5c68ae70c
Aziz Light <aziz@azizlight.me>
2014-01-03 12:15:55 +0100

    Add functionality to manage development dependencies

--- Rakefile
+++ Rakefile
@@ -7,3 +7,13 @@ Rake::TestTask.new do |t|
   t.test_files = FileList["spec/**/*_spec.rb"]
   t.verbose = true
 end
+
+namespace :gish do
+  namespace :specs do
+    task :setup do
+      gem_list = %w(fakefs mocha)
+
+      gem_list.each { |g| %x(gem install #{g}) }
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ require "minitest/autorun"
 require "minitest/pride"
 
 require "pp"
+
+def require_gem(gem)
+  begin
+    require gem
+  rescue LoadError
+    puts "`#{gem}` needed to run specs."
+    puts "Run `rake gish:specs:setup` to install"
+    puts "all the development dependencies."
+    exit 1
+  end
+end

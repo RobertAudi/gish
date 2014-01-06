03fd561c1f891b2926e8dfb83ab3b40ca149a0c5
Aziz Light <aziz@azizlight.me>
2014-01-06 09:06:57 +0100

    Fix the git commit -m wrapper

--- functions/gcm.fish
+++ functions/gcm.fish
@@ -6,21 +6,13 @@ function gcm --description="git commit with message"
     return 1
   end
 
-  set -l arguments
-
-  set -l cmd "command git diff --cached --name-only"
-
-  if test (count $ARGV) -gt 0
-    set arguments (gish find -c $cmd $ARGV)
-
-    if test -z $arguments
-      set_color red
-      echo "No matches found"
-      set_color normal
-      return 0
-    end
+  if test (count $argv) -eq 0
+    set_color red
+    echo "Arborting commit due to empty commit message"
+    set_color normal
+    return 1
   end
 
-  command git commit $OPTV $ARGV
+  command git commit -m "$argv"
   gs
 end

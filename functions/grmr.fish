4191f9fb064ff476eea24ba1b0819f16c5b6ed37
Aziz Light <aziz@azizlight.me>
2014-01-08 00:26:13 +0100

    Add git rm wrappers

---
+++ functions/grmr.fish
@@ -0,0 +1,36 @@
+function grmr --description="git rm recursively"
+  if test -z (command git rev-parse --show-toplevel 2> /dev/null)
+    set_color red
+    echo "Not in a git repository"
+    set_color normal
+    return 1
+  end
+
+  if test (count $argv) -eq 0
+    set_color red
+    echo "You need to list the files/directories to remove"
+    set_color normal
+    return 129
+  end
+
+  set -l arguments
+
+  set -l cmd "command git ls-files | command grep '/' | command sed -E 's/(.*\/).*(\..*)?/\1/g' | command sort | command uniq"
+
+  if test (count $argv) -gt 0
+    set arguments (gish find -c $cmd $argv)
+
+    # TODO: echo a message if a particular query did not match anything
+  end
+
+  if test (count $arguments) -eq 0
+    set_color red
+    echo "No matches found"
+    set_color normal
+    return 0
+  end
+
+  command git rm -r $arguments > /dev/null
+
+  gs
+end
